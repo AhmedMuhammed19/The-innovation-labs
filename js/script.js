6decbb238169bbd741cd697151f99d22fac9cf03
ahmed <ahmedmobuss@gmail.com>
2023-06-12 09:36:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let span = document.querySelector(".up");
+
+window.onscroll = function () {
+  this.scrollY >= 1000
+    ? span.classList.add("show")
+    : span.classList.remove("show");
+};
+
+span.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+let el = document.querySelector(".scroller");
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+console.log(document.documentElement.scrollHeight);
+console.log(document.documentElement.clientHeight);
+console.log(height);
+
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  el.style.width = `${(scrollTop / height) * 100}%`;
+});
